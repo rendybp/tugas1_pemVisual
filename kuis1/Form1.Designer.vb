988b0ce1c166232e88121a6baffeebda13f0b2d6
rendybp <rendibuanap@gmail.com>
2023-03-31 10:48:52 +0700

    Membuat tugas 1 atau kuis menghitung sewa mobil dan menampilkan di output lengkap

---
+++ kuis1/Form1.Designer.vb
@@ -0,0 +1,400 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.totalOutput = New System.Windows.Forms.Label()
+        Me.hariOutput = New System.Windows.Forms.Label()
+        Me.hargaOutput = New System.Windows.Forms.Label()
+        Me.namaMobilOutput = New System.Windows.Forms.Label()
+        Me.alamatOutput = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.telpOutput = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.noDaftarOutput = New System.Windows.Forms.Label()
+        Me.namaOutput = New System.Windows.Forms.Label()
+        Me.namaMobilInput = New System.Windows.Forms.TextBox()
+        Me.alamatInput = New System.Windows.Forms.TextBox()
+        Me.telpInput = New System.Windows.Forms.TextBox()
+        Me.namaInput = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.hargaInput = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.hariInput = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.totalInput = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.btnHitung = New System.Windows.Forms.Button()
+        Me.btnHasil = New System.Windows.Forms.Button()
+        Me.btnUlangi = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.totalOutput)
+        Me.GroupBox1.Controls.Add(Me.hariOutput)
+        Me.GroupBox1.Controls.Add(Me.hargaOutput)
+        Me.GroupBox1.Controls.Add(Me.namaMobilOutput)
+        Me.GroupBox1.Controls.Add(Me.alamatOutput)
+        Me.GroupBox1.Controls.Add(Me.Label9)
+        Me.GroupBox1.Controls.Add(Me.telpOutput)
+        Me.GroupBox1.Controls.Add(Me.Label10)
+        Me.GroupBox1.Controls.Add(Me.noDaftarOutput)
+        Me.GroupBox1.Controls.Add(Me.namaOutput)
+        Me.GroupBox1.Location = New System.Drawing.Point(661, 215)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(318, 348)
+        Me.GroupBox1.TabIndex = 19
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Kesimpulan"
+        '
+        'totalOutput
+        '
+        Me.totalOutput.AutoSize = True
+        Me.totalOutput.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.totalOutput.Location = New System.Drawing.Point(24, 306)
+        Me.totalOutput.Name = "totalOutput"
+        Me.totalOutput.Size = New System.Drawing.Size(17, 20)
+        Me.totalOutput.TabIndex = 16
+        Me.totalOutput.Text = "  "
+        '
+        'hariOutput
+        '
+        Me.hariOutput.AutoSize = True
+        Me.hariOutput.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.hariOutput.Location = New System.Drawing.Point(24, 260)
+        Me.hariOutput.Name = "hariOutput"
+        Me.hariOutput.Size = New System.Drawing.Size(17, 20)
+        Me.hariOutput.TabIndex = 15
+        Me.hariOutput.Text = "  "
+        '
+        'hargaOutput
+        '
+        Me.hargaOutput.AutoSize = True
+        Me.hargaOutput.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.hargaOutput.Location = New System.Drawing.Point(24, 213)
+        Me.hargaOutput.Name = "hargaOutput"
+        Me.hargaOutput.Size = New System.Drawing.Size(17, 20)
+        Me.hargaOutput.TabIndex = 14
+        Me.hargaOutput.Text = "  "
+        '
+        'namaMobilOutput
+        '
+        Me.namaMobilOutput.AutoSize = True
+        Me.namaMobilOutput.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.namaMobilOutput.Location = New System.Drawing.Point(24, 170)
+        Me.namaMobilOutput.Name = "namaMobilOutput"
+        Me.namaMobilOutput.Size = New System.Drawing.Size(17, 20)
+        Me.namaMobilOutput.TabIndex = 13
+        Me.namaMobilOutput.Text = "  "
+        '
+        'alamatOutput
+        '
+        Me.alamatOutput.AutoSize = True
+        Me.alamatOutput.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.alamatOutput.Location = New System.Drawing.Point(24, 124)
+        Me.alamatOutput.Name = "alamatOutput"
+        Me.alamatOutput.Size = New System.Drawing.Size(17, 20)
+        Me.alamatOutput.TabIndex = 12
+        Me.alamatOutput.Text = "  "
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.Label9.Location = New System.Drawing.Point(24, 170)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(17, 20)
+        Me.Label9.TabIndex = 13
+        Me.Label9.Text = "  "
+        '
+        'telpOutput
+        '
+        Me.telpOutput.AutoSize = True
+        Me.telpOutput.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.telpOutput.Location = New System.Drawing.Point(24, 77)
+        Me.telpOutput.Name = "telpOutput"
+        Me.telpOutput.Size = New System.Drawing.Size(17, 20)
+        Me.telpOutput.TabIndex = 11
+        Me.telpOutput.Text = "  "
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.Label10.Location = New System.Drawing.Point(24, 124)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(17, 20)
+        Me.Label10.TabIndex = 12
+        Me.Label10.Text = "  "
+        '
+        'noDaftarOutput
+        '
+        Me.noDaftarOutput.AutoSize = True
+        Me.noDaftarOutput.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.noDaftarOutput.Location = New System.Drawing.Point(24, 77)
+        Me.noDaftarOutput.Name = "noDaftarOutput"
+        Me.noDaftarOutput.Size = New System.Drawing.Size(17, 20)
+        Me.noDaftarOutput.TabIndex = 11
+        Me.noDaftarOutput.Text = "  "
+        '
+        'namaOutput
+        '
+        Me.namaOutput.AutoSize = True
+        Me.namaOutput.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.namaOutput.Location = New System.Drawing.Point(24, 31)
+        Me.namaOutput.Name = "namaOutput"
+        Me.namaOutput.Size = New System.Drawing.Size(17, 20)
+        Me.namaOutput.TabIndex = 10
+        Me.namaOutput.Text = "  "
+        '
+        'namaMobilInput
+        '
+        Me.namaMobilInput.Location = New System.Drawing.Point(249, 342)
+        Me.namaMobilInput.Name = "namaMobilInput"
+        Me.namaMobilInput.Size = New System.Drawing.Size(357, 26)
+        Me.namaMobilInput.TabIndex = 18
+        '
+        'alamatInput
+        '
+        Me.alamatInput.Location = New System.Drawing.Point(249, 271)
+        Me.alamatInput.Name = "alamatInput"
+        Me.alamatInput.Size = New System.Drawing.Size(357, 26)
+        Me.alamatInput.TabIndex = 17
+        '
+        'telpInput
+        '
+        Me.telpInput.Location = New System.Drawing.Point(249, 206)
+        Me.telpInput.Name = "telpInput"
+        Me.telpInput.Size = New System.Drawing.Size(357, 26)
+        Me.telpInput.TabIndex = 16
+        '
+        'namaInput
+        '
+        Me.namaInput.Location = New System.Drawing.Point(249, 144)
+        Me.namaInput.Name = "namaInput"
+        Me.namaInput.Size = New System.Drawing.Size(357, 26)
+        Me.namaInput.TabIndex = 15
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(43, 342)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(92, 20)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "Nama Mobil"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(43, 271)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(59, 20)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Alamat"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(43, 206)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(63, 20)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "No Telp"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(43, 144)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(119, 20)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Nama Penyewa"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(348, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(367, 40)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Sewa Mobil Sri Rejeki"
+        '
+        'hargaInput
+        '
+        Me.hargaInput.Location = New System.Drawing.Point(249, 403)
+        Me.hargaInput.Name = "hargaInput"
+        Me.hargaInput.Size = New System.Drawing.Size(357, 26)
+        Me.hargaInput.TabIndex = 21
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(43, 403)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(138, 20)
+        Me.Label6.TabIndex = 20
+        Me.Label6.Text = "Harga Sewa / Hari"
+        '
+        'hariInput
+        '
+        Me.hariInput.Location = New System.Drawing.Point(249, 468)
+        Me.hariInput.Name = "hariInput"
+        Me.hariInput.Size = New System.Drawing.Size(357, 26)
+        Me.hariInput.TabIndex = 23
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(43, 468)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(93, 20)
+        Me.Label7.TabIndex = 22
+        Me.Label7.Text = "Jumlah Hari"
+        '
+        'totalInput
+        '
+        Me.totalInput.Enabled = False
+        Me.totalInput.Location = New System.Drawing.Point(249, 537)
+        Me.totalInput.Name = "totalInput"
+        Me.totalInput.Size = New System.Drawing.Size(357, 26)
+        Me.totalInput.TabIndex = 25
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(43, 537)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(89, 20)
+        Me.Label8.TabIndex = 24
+        Me.Label8.Text = "Total Bayar"
+        '
+        'btnHitung
+        '
+        Me.btnHitung.Location = New System.Drawing.Point(661, 140)
+        Me.btnHitung.Name = "btnHitung"
+        Me.btnHitung.Size = New System.Drawing.Size(75, 55)
+        Me.btnHitung.TabIndex = 26
+        Me.btnHitung.Text = "Hitung"
+        Me.btnHitung.UseVisualStyleBackColor = True
+        '
+        'btnHasil
+        '
+        Me.btnHasil.Location = New System.Drawing.Point(742, 140)
+        Me.btnHasil.Name = "btnHasil"
+        Me.btnHasil.Size = New System.Drawing.Size(75, 55)
+        Me.btnHasil.TabIndex = 27
+        Me.btnHasil.Text = "Hasil"
+        Me.btnHasil.UseVisualStyleBackColor = True
+        '
+        'btnUlangi
+        '
+        Me.btnUlangi.Location = New System.Drawing.Point(823, 140)
+        Me.btnUlangi.Name = "btnUlangi"
+        Me.btnUlangi.Size = New System.Drawing.Size(75, 55)
+        Me.btnUlangi.TabIndex = 28
+        Me.btnUlangi.Text = "Ulangi"
+        Me.btnUlangi.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(904, 140)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 55)
+        Me.btnExit.TabIndex = 29
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1017, 703)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnUlangi)
+        Me.Controls.Add(Me.btnHasil)
+        Me.Controls.Add(Me.btnHitung)
+        Me.Controls.Add(Me.totalInput)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.hariInput)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.hargaInput)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.namaMobilInput)
+        Me.Controls.Add(Me.alamatInput)
+        Me.Controls.Add(Me.telpInput)
+        Me.Controls.Add(Me.namaInput)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents noDaftarOutput As System.Windows.Forms.Label
+    Friend WithEvents namaOutput As System.Windows.Forms.Label
+    Friend WithEvents namaMobilInput As System.Windows.Forms.TextBox
+    Friend WithEvents alamatInput As System.Windows.Forms.TextBox
+    Friend WithEvents telpInput As System.Windows.Forms.TextBox
+    Friend WithEvents namaInput As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents hargaInput As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents hariInput As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents totalInput As System.Windows.Forms.TextBox
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents totalOutput As System.Windows.Forms.Label
+    Friend WithEvents hariOutput As System.Windows.Forms.Label
+    Friend WithEvents hargaOutput As System.Windows.Forms.Label
+    Friend WithEvents namaMobilOutput As System.Windows.Forms.Label
+    Friend WithEvents alamatOutput As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents telpOutput As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents btnHitung As System.Windows.Forms.Button
+    Friend WithEvents btnHasil As System.Windows.Forms.Button
+    Friend WithEvents btnUlangi As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ kuis1/Form1.vb
@@ -0,0 +1,49 @@
+﻿Public Class Form1
+
+    Private Sub btnExit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnExit.Click
+        End
+    End Sub
+
+    Private Sub btnHasil_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnHasil.Click
+        namaOutput.Text = namaInput.Text
+        telpOutput.Text = telpInput.Text
+        alamatOutput.Text = alamatInput.Text
+        namaMobilOutput.Text = namaMobilInput.Text
+        hargaOutput.Text = hargaInput.Text
+        hariOutput.Text = hariInput.Text
+        totalOutput.Text = totalInput.Text
+    End Sub
+
+    Private Sub btnUlangi_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnUlangi.Click
+        namaInput.Text = " "
+        telpInput.Text = " "
+        alamatInput.Text = " "
+        namaMobilInput.Text = " "
+        hargaInput.Text = " "
+        hariInput.Text = " "
+        totalInput.Text = " "
+
+        namaOutput.Text = " "
+        telpOutput.Text = " "
+        alamatOutput.Text = " "
+        namaMobilOutput.Text = " "
+        hargaOutput.Text = " "
+        hariOutput.Text = " "
+        totalOutput.Text = " "
+
+        namaInput.Focus()
+    End Sub
+
+    Private Sub btnHitung_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnHitung.Click
+        Dim harga As String
+        Dim hari As Integer
+        Dim total As String
+
+        harga = CInt(hargaInput.Text)
+        hari = CInt(hariInput.Text)
+
+        total = (harga * hari)
+        totalInput.Text = "Rp. " & total
+    End Sub
+
+End Class
